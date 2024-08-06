42b6061102bb4c2e2758467f50aa0202d6e7dc6b
Baishnawi Agrawal <145450500+Baish34@users.noreply.github.com>
2024-08-06 08:51:31 +0000

    created slice and added category reducers

--- src/features/categories/categoriesSlice.js
+++ src/features/categories/categoriesSlice.js
@@ -10,3 +10,29 @@ export const fetchCategories = createAsyncThunk(
     return response.data;
   },
 );
+
+const categoriesSlice = createSlice({
+  name: "categories",
+  initialState: {
+    categories: [],
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCategories.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.categories = action.payload;
+      })
+      .addCase(fetchCategories.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default categoriesSlice.reducer;

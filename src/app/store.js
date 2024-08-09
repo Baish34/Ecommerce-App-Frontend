e9cf84e16881e99ac2fee8bd3a670afde5ea532c
Baishnawi Agrawal <145450500+Baish34@users.noreply.github.com>
2024-08-09 05:34:26 +0000

    Imorted wishlistreducer in store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoriesReducer from "../features/categories/categoriesSlice";
 import productsReducer from "../features/products/productsSlice";
+import wishlistReducer from '../features/wishlist/wishlistSlice';
 
 export default configureStore({
   reducer: {
     categories: categoriesReducer,
     products: productsReducer,
+    wishlist: wishlistReducer,
   },
 });

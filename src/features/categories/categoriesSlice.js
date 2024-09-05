920e0a24a289d8fac250f4ddb72acd1b5044890f
Baishnawi Agrawal <145450500+Baish34@users.noreply.github.com>
2024-09-05 05:56:19 +0000

    Added vercel link

--- src/features/categories/categoriesSlice.js
+++ src/features/categories/categoriesSlice.js
@@ -5,7 +5,7 @@ export const fetchCategories = createAsyncThunk(
   "categories/fetchCategories",
   async () => {
     const response = await axios.get(
-      "https://c7c43001-8c92-4c4e-a0de-050eac64989a-00-31tqe0fdcvd73.picard.replit.dev/api/categories",
+      "ecommerce-app-backend-3rijbqvbc.vercel.app/api/categories",
     );
     return response.data;
   },
